5f75220b35b83c61493906abdcd882943b72a84e
Pedro Batista <pedro.batista@triasoftware.com.br>
2019-04-11 16:16:20 -0300

    Curso Angular - 11/03/2019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,12 @@ import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
 import { RestaurantsService } from './restaurants/restaurants.service';
-
-
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart-service';
 
 
 @NgModule({
@@ -23,14 +27,19 @@ import { RestaurantsService } from './restaurants/restaurants.service';
     HomeComponent,
     AboutComponent,
     RestaurantsComponent,
-    RestaurantComponent
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    ShoppingCartComponent,
+    MenuItemComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService],
+  providers: [RestaurantsService, ShoppingCartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,28 +1,42 @@
-import { Restaurant } from "./restaurant/restaurant.model";
+import {Injectable} from '@angular/core'
+import {Http} from '@angular/http'
+import {Observable} from 'rxjs/observable'
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
+import {Restaurant} from "./restaurant/restaurant.model";
+import {MEAT_API} from "../app.api"
+import {ErrorHandler} from 'app/app.error-handler';
+import {MenuItem} from 'app/restaurant-detail/menu-item/menu-item-model';
 
+
+@Injectable()
 export class RestaurantsService {
 
-    rests: Restaurant[] =  [
-        {
-          id: "bread-bakery",
-          name: "Bread & Bakery",
-          category: "Bakery",
-          deliveryEstimate: "25m",
-          rating: 4.9,
-          imagePath: "assets/img/restaurants/breadbakery.png"
-        },
-        {
-          id: "burger-house",
-          name: "Burger House",
-          category: "Hamburgers",
-          deliveryEstimate: "100m",
-          rating: 3.5,
-          imagePath: "assets/img/restaurants/burgerhouse.png",
-        }]
-    
-    constructor(){}
+    constructor(private http: Http) {}
 
-    restaurants(): Restaurant[] {
-        return this.rests;
+    restaurants(): Observable<Restaurant[]> {
+      return this.http.get(`${MEAT_API}/restaurants`)
+      .map(response => response.json())
+      ._catch(ErrorHandler.handleError)
     }
+
+    restaurantsById(id: string): Observable<Restaurant> {
+      return this.http.get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      ._catch(ErrorHandler.handleError)
+    }
+
+    reviewsOfRestaurant(id: string) : Observable<any>{
+      return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+      .map(response => response.json())
+      ._catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurant(id: string) : Observable<MenuItem[]>{
+      return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+      .map(response => response.json())
+      ._catch(ErrorHandler.handleError)
+    }
+    
+
 }
