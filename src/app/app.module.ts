65006fce22f9115739a4979b91bf411c97a87ff0
pedro.batista <peehid@gmail.com>
2019-04-23 00:37:48 -0300

    Aula 93 - Curso Angular
    Lazy Loading, Core Module, e Module Compartilhado & Animações com Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 
 import {ROUTES} from './app.routes'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestaurantsService } from './restaurants/restaurants.service';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart-service';
-import { OrderComponent } from './order/order.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component';
-import { OrderService } from './order/order.service';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
-import { RatingComponent } from './shared/rating/rating.component';
-
-
+import { SharedModule } from './shared/shared.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent,
     RestaurantsComponent,
     RestaurantComponent,
     RestaurantDetailComponent,
@@ -43,22 +31,16 @@ import { RatingComponent } from './shared/rating/rating.component';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent,
-    InputComponent,
-    RadioComponent,
-    OrderItemsComponent,
-    DeliveryCostsComponent,
     OrderSummaryComponent,
-    RatingComponent
   ],
   imports: [
-    FormsModule,
     BrowserModule,
+    BrowserAnimationsModule,
     HttpModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot(ROUTES)
+    SharedModule.forRoot(),
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
-  providers: [RestaurantsService, ShoppingCartService, OrderService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingCartService } from './shopping-cart-service';
+import { style, state, transition,animate, trigger, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'mt-shopping-cart',
-  templateUrl: './shopping-cart.component.html'
+  templateUrl: './shopping-cart.component.html',
+  animations: [
+    trigger('row', [
+      state('ready', style({opacity:1})),
+      transition('void => ready' , animate('300ms 0s ease-in', keyframes([
+        style({opacity:0, transform: 'translateX(-30opx)', offset:0}),
+        style({opacity:0.6, transform: 'translateX(10opx)', offset:0.6}),
+        style({opacity:1, transform: 'translateX(0opx)', offset:1})
+      ]))),
+      transition('ready => void' , animate('300ms 0s ease-out', keyframes([
+        style({opacity:1, transform: 'translateX(0opx)', offset:0}),
+        style({opacity:0.4, transform: 'translateX(-10opx)', offset:0.4}),
+        style({opacity:0, transform: 'translateX(30opx)', offset:1})
+      ])))
+    ])
+
+  ]
 })
 export class ShoppingCartComponent implements OnInit {
+
+  rowState = 'ready'
 
   constructor(private shoppingCartService: ShoppingCartService) { }
 
